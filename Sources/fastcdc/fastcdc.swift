fcd9cd3293cd1dec7be5a3e4f0c2f137edee602b
glasfisch3000 <jakob@danckwerts.net>
2024-02-17 17:57:31 +0100

    added sequential split method

--- Sources/fastcdc/fastcdc.swift
+++ Sources/fastcdc/fastcdc.swift
@@ -28,3 +28,38 @@ public func fastCDCSplit<Source: CDCSource>(_ source: Source, minSize: Int, avgS
     
     return .notFound(offset+i)
 }
+
+public enum SequentialSplitResult<Source: CDCSource> {
+    case tooSmall
+    case found(_ processed: [Source])
+    case notFound(_ processed: [Source])
+}
+
+public func fastCDCSplit<Source: CDCSource>(_ sources: [Source], minSize: Int, avgSize: Int, maxSize: Int) -> SequentialSplitResult<Source> {
+    let log = UInt(avgSize.bitWidth - avgSize.leadingZeroBitCount)
+    let maskS: UInt = (1 << (log-2)) - 1
+    let maskL: UInt = (1 << (log+2)) - 1
+    
+    var hash: UInt = 0
+    
+    var processed: [Source] = []
+    var count = 0
+    
+    for source in sources {
+        guard count >= minSize else { continue }
+        guard count + source.count <= maxSize else { break }
+        
+        for byte in source.makeSubsequence(from: 0, length: source.count) {
+            hash <<= 1
+            hash += table[Int(byte)]
+            
+            if hash & (count < avgSize ? maskS : maskL) == 0 { return .found(processed) }
+            
+            count += 1
+        }
+        
+        processed.append(source)
+    }
+    
+    return .notFound(processed)
+}
