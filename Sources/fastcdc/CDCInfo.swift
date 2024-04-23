430f848663f3eddb17329f04c4ae1a0e888c8fd0
glasfisch3000 <jakob@danckwerts.net>
2024-04-23 02:06:11 +0200

    added CDCInfo initializer

--- Sources/fastcdc/CDCInfo.swift
+++ Sources/fastcdc/CDCInfo.swift
@@ -2,4 +2,10 @@ public struct CDCInfo {
     public var minBytes: Int
     public var avgBytes: Int
     public var maxBytes: Int
+    
+    public init(min: Int, avg: Int, max: Int) {
+        self.minBytes = min
+        self.avgBytes = avg
+        self.maxBytes = max
+    }
 }
