b51bfb93eeb9a39b71b41fa6850a005ec4a51b2c
glasfisch3000 <jakob@danckwerts.net>
2024-05-06 23:59:05 +0200

    fixed data algorithm overflow if too small

--- Sources/fastcdc/implementation-data.swift
+++ Sources/fastcdc/implementation-data.swift
@@ -27,25 +27,21 @@ extension Data.Chunked {
         let maskS: UInt = (1 << (log-2)) - 1
         let maskL: UInt = (1 << (log+2)) - 1
         
-        var byteCount = 1
+        var byteCount = 0
         var hash = self.seed
         
         let status = source.withUnsafeBytes { bytes -> CDCBreakpointType in
             for index in self.index ..< endIndex {
+                byteCount += 1
                 guard byteCount < info.maxBytes else { return .tooLarge }
                 
                 let mask = byteCount < info.avgBytes ? maskS : maskL
                 hash <<= 1
                 (hash, _) = hash.addingReportingOverflow(table[Int(bytes[index])])
                 
-                guard byteCount >= info.minBytes else {
-                    byteCount += 1
-                    continue
-                }
+                guard byteCount >= info.minBytes else { continue }
                 
                 if hash & mask == 0 { return .split }
-                
-                byteCount += 1
             }
             
             return byteCount >= info.minBytes ? .notFound : .tooSmall
